680122f628df7fdd2756bcf4eb74096d9c694779
timauk27 <timauk27@users.noreply.github.com>
2026-02-20 01:48:02 -0300

    feat: install google tag manager container GTM-PQ59T8BN

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Script from "next/script";
+
 import {
   Geist,
   Geist_Mono,
@@ -84,6 +86,27 @@ export default function RootLayout({
         `}
       >
         <ThemeProvider>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-PQ59T8BN"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+          {/* End Google Tag Manager (noscript) */}
+
+          <Script id="gtm" strategy="afterInteractive">
+            {`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-PQ59T8BN');
+              `}
+          </Script>
+
 
           <MegaHeader />
           {children}
